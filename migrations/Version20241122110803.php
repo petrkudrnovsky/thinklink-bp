e4da70d8c7ff56d180f75657bdf6b137930156f7
Petr Kudrnovský <kudrnovsky.p@gmail.com>
2024-12-20 21:09:13 +0100

    Add CRUD, fix delete form and translate to Czech

--- migrations/Version20241122110803.php
+++ migrations/Version20241122110803.php
@@ -21,7 +21,7 @@ final class Version20241122110803 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE note (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, content TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN note.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN notes.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
         $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
--- src/Form/UploadNoteType.php
+++ src/Form/UploadNoteType.php
@@ -4,21 +4,24 @@ namespace App\Form;
 
 use App\Entity\Note;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class NoteType extends AbstractType
+class UploadNoteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, [
-                'label' => 'Název',
+            ->add('files', FileType::class, [
+                'label' => 'Nahrajte Markdown soubory',
+                'multiple' => true,
+                'mapped' => false,
+                'required' => true,
             ])
-            ->add('content', TextareaType::class, [
-                'label' => 'Obsah',
+            ->add('save', SubmitType::class, [
+                'label' => 'Nahrát',
             ])
         ;
     }
