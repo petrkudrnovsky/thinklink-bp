51cb2bf5e9c0ec217b38162f3efef9d9378045b1
Petr Kudrnovský <kudrnovsky.p@gmail.com>
2024-12-20 21:09:13 +0100

    Add CRUD, fix delete form and translate to Czech

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Note;
+use App\Form\UploadNoteType;
 use App\Form\NoteType;
 use App\Repository\NoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,21 +13,41 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class NoteController extends AbstractController
+#[Route('/note')]
+final class NoteController extends AbstractController
 {
-    #[Route('/note', name: 'app_note')]
-    public function index(NoteRepository $repository): Response
+    #[Route(name: 'app_note_index')]
+    public function index(NoteRepository $noteRepository): Response
     {
-        $notes = $repository->findAll();
         return $this->render('note/index.html.twig', [
-            'notes' => $notes,
+            'notes' => $noteRepository->findAll(),
         ]);
     }
 
-    #[Route('/note/new', name: 'app_note_new')]
-    public function new(Request $request, EntityManagerInterface $em): Response
+    #[Route('/new', name: 'app_note_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(NoteType::class);
+        $note = new Note();
+        $form = $this->createForm(NoteType::class, $note);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($note);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_note_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('note/new.html.twig', [
+            'note' => $note,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/upload', name: 'app_note_upload')]
+    public function uploadNote(Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(UploadNoteType::class);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -46,41 +67,49 @@ class NoteController extends AbstractController
 
             $em->flush();
 
-            return $this->redirectToRoute('app_note');
+            return $this->redirectToRoute('app_note_index');
         }
 
-        return $this->render('note/new.html.twig', [
+        return $this->render('note/upload.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/note/{id}', name: 'app_note_show')]
-    public function show(int $id, NoteRepository $repository): Response
+
+    #[Route('/{id}', name: 'app_note_show', methods: ['GET'])]
+    public function show(Note $note): Response
     {
-        $note = $repository->findById($id);
-
-        if (!$note) {
-            throw $this->createNotFoundException('Note not found.');
-        }
-
         return $this->render('note/show.html.twig', [
             'note' => $note,
         ]);
     }
 
-    #[Route('/note/{id}/edit', name: 'app_note_edit')]
-    public function edit(int $id): Response
+    #[Route('/{id}/edit', name: 'app_note_edit')]
+    public function edit(Request $request, Note $note, EntityManagerInterface $entityManager): Response
     {
+        $form = $this->createForm(NoteType::class, $note);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_note_index', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('note/edit.html.twig', [
-            'controller_name' => 'NoteController',
-            'id' => $id,
+            'note' => $note,
+            'form' => $form,
         ]);
     }
 
-    #[Route('/note/{id}', name: 'app_note_delete', methods: ['DELETE'])]
-    public function delete(int $id): Response
+    #[Route('/{id}', name: 'app_note_delete', methods: ['POST'])]
+    public function delete(Request $request, Note $note, EntityManagerInterface $entityManager): Response
     {
-        // to-do: delete note
-        return $this->redirectToRoute('app_note');
+        if ($this->isCsrfTokenValid('delete'.$note->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($note);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_note_index', [], Response::HTTP_SEE_OTHER);
     }
 }
