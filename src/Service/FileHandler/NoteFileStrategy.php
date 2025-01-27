8d898d21d345a144435e47f9be9113a4fc3d776d
Petr Kudrnovský <kudrnovsky.p@gmail.com>
2025-01-27 13:06:21 +0100

    Add NoteStrategy and unify uploading files in the app

---
+++ src/Service/FileHandler/NoteFileStrategy.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Service\FileHandler;
+
+use App\Entity\FilesystemFile;
+use App\Entity\Note;
+use App\Repository\NoteRepository;
+use App\Service\SlugGenerator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class NoteFileStrategy implements FileHandlerStrategyInterface
+{
+    private static int $MAX_NOTE_SIZE = 5000000;
+    public function __construct(
+        // injected from services.yaml
+        private array $allowedMimeTypes,
+        private SlugGenerator $slugGenerator,
+        private NoteRepository $noteRepository,
+    )
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supports(UploadedFile $file): bool
+    {
+        return in_array($file->getMimeType(), $this->allowedMimeTypes);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function upload(UploadedFile $file, EntityManagerInterface $em): void
+    {
+        $note = new Note(
+            htmlspecialchars(pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME)),
+            $this->slugGenerator->generateUniqueSlug(pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME)),
+            htmlspecialchars(file_get_contents($file->getPathname())),
+            new \DateTimeImmutable()
+        );
+
+        $em->persist($note);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validate(UploadedFile $file, ExecutionContextInterface $context): void
+    {
+        if($file->getSize() > self::$MAX_NOTE_SIZE) {
+            $context->buildViolation('Poznámka: ' . htmlspecialchars($file->getClientOriginalName()) . ' je příliš velká. Maximální povolená velikost je ' . self::$MAX_NOTE_SIZE . ' bajtů.')
+                ->atPath('files')
+                ->addViolation();
+            return;
+        }
+
+        $futureNoteTitle = htmlspecialchars(pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME));
+        if(strlen($futureNoteTitle) > 255) {
+            $context->buildViolation('Poznámka: ' . $futureNoteTitle . ' má příliš dlouhý název. Maximální povolená délka je 255 znaků.')
+                ->atPath('files')
+                ->addViolation();
+            return;
+        }
+
+        $note = $this->noteRepository->findOneBy(['title' => $futureNoteTitle]);
+        if($note) {
+            $context->buildViolation('Název poznámky (' . $note->getTitle() . ') je již použitý. Prosím pojmenujte soubor jinak.')
+                ->atPath('files')
+                ->addViolation();
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsServe(FilesystemFile $file): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function serve(FilesystemFile $file, string $disposition = "inline"): Response
+    {
+        throw new \LogicException('This strategy does not support serving files');
+    }
+}
